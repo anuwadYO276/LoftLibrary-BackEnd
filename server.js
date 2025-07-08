f64479bad7617a480f0adcd4c99928fbeb5e0834
Yo <158416918+anuwadYO276@users.noreply.github.com>
2025-07-08 23:49:58 +0700

    เพิ่ม BASIC_AUTH

--- server.js
+++ server.js
@@ -1,23 +1,40 @@
 import dotenv from "dotenv";
 dotenv.config();
 
-import express from "express"
-import morgan from "morgan"
-import cors from "cors"
-import { readdirSync } from "fs"
+import express from "express";
+import morgan from "morgan";
+import cors from "cors";
+import { readdirSync } from "fs";
+import basicAuth from "basic-auth";
 
+const app = express();
 
-const app = express()
-app.use(morgan("dev"))
-app.use(express.json())
-app.use(cors())
-app.use(express.static("public"))
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(cors());
+app.use(express.static("public"));
+
+// Basic Auth middleware
+const authMiddleware = (req, res, next) => {
+  const user = basicAuth(req);
+
+  const USERNAME = process.env.BASIC_AUTH_USER;
+  const PASSWORD = process.env.BASIC_AUTH_PASS;
+
+  if (!user || user.name !== USERNAME || user.pass !== PASSWORD) {
+    res.set("WWW-Authenticate", 'Basic realm="401"');
+    return res.status(401).send("Authentication required.");
+  }
+  next();
+};
+
+app.use(authMiddleware);
 
 readdirSync("./routes").forEach(async (r) => {
-  const route = await import(`./routes/${r}`)
-  app.use("", route.default)
-})
+  const route = await import(`./routes/${r}`);
+  app.use("", route.default);
+});
 
 app.listen(process.env.PORT || 3000, () =>
-    console.log('server is running')
-)
+  console.log("server is running")
+);
