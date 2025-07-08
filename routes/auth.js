7ae5e4ebbca2332a763ceb39b1f918621fde9935
Yo <158416918+anuwadYO276@users.noreply.github.com>
2025-07-08 23:32:36 +0700

    เพิ่มเส้น การ login and sigup แบบ และ เพิ่มการทำ docker

--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,29 @@
 import express from "express"
 const router = express.Router()
-import { signupReader,signupWriter, login } from "../controller/auth.js"
+import { 
+    signupReader
+    ,signupWriter
+    , login 
+    , signupReaderGoogle
+    , signupReaderFacebook
+    , signupWriterGoogle
+    , signupWriterFacebook
+    , loginSocialUser
+    , forgotPassword
+    , resetPassword
+    , updateUserProfile
+} from "../controller/auth.js"
 
 router.post("/signup/reader", signupReader)
 router.post("/signup/writer", signupWriter)
 router.post("/login", login)
+router.post("/forgot-password", forgotPassword)
+router.post("/reset-password", resetPassword)
+router.post("/signup/reader_google", signupReaderGoogle)
+router.post("/signup/reader_facebook", signupReaderFacebook)
+router.post("/signup/writer_google", signupWriterGoogle)
+router.post("/signup/writer_facebook", signupWriterFacebook)
+router.post("/login/social", loginSocialUser)
+router.post("/update-profile", updateUserProfile)
 
 export default router
